34e2739d5d5f3278a128f3a72ff8a02092c57287
yusya23 <44222839+yusya23@users.noreply.github.com>
2019-02-06 13:39:14 +0900

    Add files via upload

--- keras_RNN.py
+++ keras_RNN.py
@@ -90,16 +90,201 @@ history = model.fit(x_train, y_train,
                     batch_size=32,
                     validation_split=0.2)
 
+
+##########textの分類################
 #学習済みの単語埋め込み
 import os
 import pandas as pd
 import codecs
+import numpy as np
 
 os.chdir("C:\\Users\\kagitani\\Documents\\keras_DL")
 #Imdb_data = pd.read_csv('imdb_master.csv', encoding='shift-jis')
 #文字コードエラーが起こるので以下を使用
 with codecs.open("imdb_master.csv", "r", "Shift-JIS", "ignore") as file:
     df = pd.read_table(file, delimiter=",")
-    
-    
+#ラベルを0,1表記に
+labels = []
+for i in df['label']:
+    if i == 'pos':
+        labels.append(1)
+    elif i == 'neg':
+        labels.append(0)
+    else:
+        labels.append(2)
+#0,1表記でないものを削除
+df['label_0_1'] = pd.DataFrame(labels)
+df = df.drop(df[df['label_0_1']==2].index)
+df = df.drop(['Unnamed: 0', 'label', 'file', 'type'], axis=1)
 
+labels = list(df['label_0_1'])
+texts = list(df['review'])
+
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+
+max_len = 100                #映画レヴューを100ワードでカット
+training_samples = 200       #200個のサンプルで訓練
+validation_samples = 10000   #10,000個のサンプルで検証
+max_words = 10000            #データセットの最初から10,000ワードのみを考慮  
+
+tokenizer = Tokenizer(num_words=max_words)
+tokenizer.fit_on_texts(texts)
+sequences = tokenizer.texts_to_sequences(texts)
+
+word_index = tokenizer.word_index
+print('Found %s unique tokens.' % len(word_index))
+
+data = pad_sequences(sequences, maxlen=max_len)
+
+labels = np.asarray(labels)
+print('Shape of data tensor:', data.shape)
+print('Shape of label tensor:', labels.shape)
+
+#データをシャッフルして訓練と検証にデータを分割
+indices = np.arange(data.shape[0])
+np.random.shuffle(indices)
+data = data[indices]
+labels = labels[indices]
+
+x_train = data[:training_samples]
+y_train = labels[:training_samples]
+x_val = data[training_samples: training_samples + validation_samples]
+y_val = labels[training_samples: training_samples + validation_samples]
+
+#GloVeの単語埋め込みファイルの読み込み
+glove_dir = 'C:\\Users\\kagitani\\Documents\\keras_DL\\GloVe'
+
+embeddings_index = {}
+f = open(os.path.join(glove_dir, 'glove.6B.100d.txt'), encoding='utf-8')
+for line in f:
+    values = line.split()
+    word = values[0]
+    coefs = np.asarray(values[1:], dtype='float32')
+    embeddings_index[word] = coefs
+f.close()
+
+print('Found %s word vectors.' % len(embeddings_index))
+
+#GloVe の単語埋め込み行列の準備
+embedding_dim = 100
+embedding_matrix = np.zeros((max_words, embedding_dim))
+for word, i in word_index.items():
+    embedding_vector = embeddings_index.get(word)
+    if i < max_words:
+        if embedding_vector is not None:
+            embedding_matrix[i] = embedding_vector
+#モデルの定義
+from keras.models import Sequential
+from keras.layers import Embedding, Flatten, Dense
+
+model = Sequential()
+model.add(Embedding(max_words, embedding_dim, input_length=max_len))
+model.add(Flatten())
+model.add(Dense(32, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+model.summary()
+
+#単語埋め込み行列をEmbedding層に読み込む
+model.layers[0].set_weights([embedding_matrix])
+model.layers[0].trainable = False
+
+model.compile(optimizer='rmsprop',
+              loss='binary_crossentropy',
+              metrics=['acc'])
+history = model.fit(x_train, y_train,
+                    epochs=10,
+                    batch_size=32,
+                    validation_data=(x_val, y_val))
+model.save_weights('pre_trained_glove_model.h5')
+
+
+############RNN###############
+#モデル一例
+from keras.layers import SimpleRNN
+from keras.models import Sequential
+from keras.layers import Embedding
+
+model = Sequential()
+model.add(Embedding(10000, 32))
+model.add(SimpleRNN(32, return_sequences=True))
+model.add(SimpleRNN(32, return_sequences=True))
+model.add(SimpleRNN(32, return_sequences=True))
+model.add(SimpleRNN(32))
+model.summary()
+
+#実装
+#データの準備
+from keras.datasets import imdb
+from keras.preprocessing import sequence
+
+max_features = 10000
+max_len = 500
+batch_size = 32
+
+print('Loading data...')
+(input_train, y_train), (input_test, y_test) = imdb.load_data(num_words=max_features)
+print(len(input_train), 'train sequences')
+print(len(input_test), 'test sequences')
+
+print('Pad sequences(samples x time)')
+input_train = sequence.pad_sequences(input_train, maxlen=max_len)
+input_test = sequence.pad_sequences(input_test, maxlen=max_len)
+print('input_train shape', input_train.shape)
+print('inpurt_test shape', input_test.shape)
+
+#モデル構築
+from keras.models import Sequential
+from keras.layers import Dense
+
+model = Sequential()
+model.add(Embedding(max_features, 32))
+model.add(SimpleRNN(32))
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile(optimizer='rmsprop',
+              loss='binary_crossentropy',
+              metrics=['acc'])
+history = model.fit(input_train, y_train,
+                    epochs=10, batch_size=128, validation_split=0.2)
+
+#結果をプロット
+import matplotlib.pyplot as plt
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_acc']
+
+epochs = range(len(acc))
+
+plt.plot(epochs, acc, 'bo', label='training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+
+plt.figure()
+plt.plot(epochs, loss, 'bo', label='training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()
+
+#LSTM層の使用
+from keras.layers import LSTM
+
+model = Sequential()
+model.add(Embedding(max_features, 32))
+model.add(LSTM(32))
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile(optimizer='rmsprop',
+              loss='binary_crossentropy',
+              metrics=['acc'])
+history = model.fit(input_train, y_train,
+                    epochs=10,
+                    batch_size=128,
+                    validation_split=0.2)
+
+#RNNの高度な使い方
